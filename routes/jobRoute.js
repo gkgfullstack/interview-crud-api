3ddf996765e83d6ea728b07fafca4079d0918b74
gkggreat <gkggreat@gmail.com>
2026-06-28 13:51:37 +0530

    "fix the route for job creation"

--- routes/jobRoute.js
+++ routes/jobRoute.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const { postJob, getAllJobs } = require("../controllers/jobController");
 router.post('/jobs', postJob);
-router.get("./jobs", getAllJobs);
+router.get('/jobs', getAllJobs);
 module.exports = router;
